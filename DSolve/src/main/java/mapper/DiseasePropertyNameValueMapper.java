819c3df6ee6ebcb018ea71be842b80e882b5f35d
Liubaichuan <Liubaichuan@DESKTOP-I9IN3LI>
2017-09-01 15:32:37 +0800

    第一次提交

---
+++ DSolve/src/main/java/mapper/DiseasePropertyNameValueMapper.java
@@ -0,0 +1,39 @@
+package mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import pojo.*;
+
+public interface DiseasePropertyNameValueMapper {
+	
+	@Select("select * from disease_name where disease_name = #{name}")
+	public List<DiseaseName> getDiseaseByName(String name);
+	
+	@Insert("insert into disease_name (disease_name) values (#{diseaseName})")
+	@Options(useGeneratedKeys=true, keyProperty="id", keyColumn="id")
+	public void addDiseaseName(DiseaseName bean);
+	
+	@Insert("insert into disease_property (disease_id, property_id) values (#{diseaseId}, #{propertyId})")
+	@Options(useGeneratedKeys=true, keyProperty="id", keyColumn="id")
+	public void addDiseaseProperty(DiseaseProperty bean);
+	
+	@Select("select * from property_name where property_name = #{name}")
+	public List<PropertyName> getPropertyNameByName(String name);
+	
+	@Insert("insert into property_name (property_name) values (#{propertyName})")
+	@Options(useGeneratedKeys=true, keyProperty="id", keyColumn="id")
+	public void addPropertyName(PropertyName bean);
+	
+	@Insert("insert into property_name_value (name_id, value_id) values (#{nameId}, #{valueId})")
+	@Options(useGeneratedKeys=true, keyProperty="id", keyColumn="id")
+	public void addPropertyNameValue(PropertyNameValue bean);
+	
+	@Insert("insert into property_value (disease_id, property_value) values (#{diseaseId}, #{propertyValue})")
+	@Options(useGeneratedKeys=true, keyProperty="id", keyColumn="id")
+	public void addPropertyValue(PropertyValue bean);
+
+}
---
+++ DSolve/src/main/java/pojo/DiseaseName.java
@@ -0,0 +1,29 @@
+package pojo;
+
+public class DiseaseName {
+	
+	private Integer id;
+	
+	private String diseaseName;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDiseaseName() {
+		return diseaseName;
+	}
+
+	public void setDiseaseName(String diseaseName) {
+		this.diseaseName = diseaseName;
+	}
+
+
+
+	
+
+}
